8b7e891c0fa169291722316efb55004a488aba22
kkminaaa <56380257+kkminaaa@users.noreply.github.com>
2019-10-20 20:09:57 +0900

    add messagesend2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,15 @@
 class MessagesController < ApplicationController
   before_action :set_group
+  
   def index
     @message = Message.new
-    @messages = @group.message.includes(:user)
+    @messages = @group.messages.includes(:user)
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
