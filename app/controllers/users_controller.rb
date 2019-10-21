4774ba9c89c5e388cae2651402bcd2a43fa0c223
kkminaaa <56380257+kkminaaa@users.noreply.github.com>
2019-10-21 10:25:27 +0900

    add-groupname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,13 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
   end
 end
 
-private
-
-def user_params
-  params.require(:user).permit(:name,:email)
-end
